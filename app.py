cd04fcd63c0fd535273c3fdbbc5d6213364bd2c3
superhenryman <kareem.a.alhaj@gmail.com>
2025-06-27 19:11:00 +0300

    tung tung tung tung rtung tung SAHUR!!

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def insert_post(post):
         except Exception as e:
             logging.error(f"Error occured when inserting the post contents. {e}")
 
-@app.route("/posts", methods=["POST"])
+@app.route("/posts", methods=["GET"])
 def retrieve_data():
     with get_connection() as conn:
        cursor = conn.cursor()
