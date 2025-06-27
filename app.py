1cefcf33cc76604fae2ed43929b831990e15c675
superhenryman <kareem.a.alhaj@gmail.com>
2025-06-27 17:31:40 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, jsonify
+import html
+import logging
+import psycopg2
+import os
+import time
+
+logging.basicConfig(level=logging.INFO)
+app = Flask(__name__)
+database_url = os.getenv("DATABASE_URL")
+
+def clean(text: str) -> str:
+    """ Function to prevent XSS (screw you, dirty hacker.) """
+    return str(html.escape(text), quote=True)
+
+def get_connection():
+    retry_count = 5
+    for i in range(retry_count):
+        try:
+            conn = psycopg2.connect(database_url)
+            return conn
+        except Exception as e:
+            print(f"Attempt {i} failed: {e}")
+            time.sleep(2 ** i)
+    raise Exception("I can't connect :(")
+
+def init_db():
+    try:
+        with get_connection() as conn:
+            conn = get_connection()
+            cur = conn.cursor()
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS posts(
+                id SERIAL PRIMARY KEY,
+                post TEXT NOT NULL
+            );""")
+            conn.commit()
+            cur.close()
+            conn.close()
+    except Exception as e:
+        logging.error(f"Error occured initializing the database. {e}")
+
+init_db()
+
+def insert_post(post):
+    with get_connection() as conn:
+        try:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO posts (post) VALUES (%s)", (post, ))
+            conn.commit()
+        except Exception as e:
+            logging.error(f"Error occured when inserting the post contents. {e}")
+
+@app.route("/posts")
+def retrieve_data():
+    with get_connection() as conn:
+       cursor = conn.cursor()
+       cursor.execute("SELECT * FROM orders")
+       rows = cursor.fetchall()
+       jsondata = {index: item for index, item in enumerate(rows)}
+    return jsonify(jsondata)
+
+@app.route("/post")
+def post():
+    content = request.form.get("content")
+    insert_post(clean(content))
+    return 200
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__": app.run(debug=True)
+
---
+++ static/script.js
@@ -0,0 +1,52 @@
+const postButton = document.getElementById("post");
+const errorBuffer = document.getElementById("error");
+const sendPostButton = document.getElementById("post-button");
+function showPostGUI() {
+    document.getElementById('post-gui').style.display = "block";
+    document.getElementById("post-form").style.display =  "flex";
+    document.getElementById("content").style.display = "block";
+    postButton.style.display = "none";
+}
+
+function checkPostGUIOpen() {
+    if (document.getElementById("post-gui").style.display === "block" && document.getElementById("post-form").style.display === "flex" && document.getElementById("content").style.display === "block" && postButton.style.display === "none") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function closePostGUI() {
+    document.getElementById('post-gui').style.display = "none";
+    document.getElementById("post-form").style.display =  "none";
+    document.getElementById("content").style.display = "none";
+    postButton.style.display = ""; // if it works don't touch it lol
+}
+
+
+
+function handleError(error) {
+    errorBuffer.innerText = `${error}`;
+    errorBuffer.style.display = "block";
+}
+
+async function getPosts() {
+    try {
+        const rawData = await fetch("/posts", {
+            method: "GET"
+        });
+        const json = await rawData.json();
+        
+    } catch (err) {
+        console.log("Failed to fetch posts:", err);
+        handleError(`Failed to retrieve posts, error: ${err}`);
+        return;
+    }
+    
+}
+document.addEventListener("DOMContentLoaded", () => {
+    //getPosts();
+    document.getElementById("back").addEventListener("click", function(){closePostGUI();});
+    postButton.addEventListener('click', function (){showPostGUI();});
+
+});
